8f4e387d422ddf125f3fadef7388381ca2bf0168
Vitor Henckel <vitor.henckel@leadforce.com.br>
2017-06-14 13:52:13 -0300

    Adicionado método de pontuação por porcentagem

--- controllers/Coin.php
+++ controllers/Coin.php
@@ -37,6 +37,9 @@ class Coin extends CI_Controller {
 
     public function neural_calculo()
     {
+        $percent = 0.02; // Percentual para efetuar compra e venda
+        $qtd     = 1;    // Quantidade a ser negociada em cada transação
+
         $subindo  = 0;
         $descendo = 0;
 
@@ -44,14 +47,21 @@ class Coin extends CI_Controller {
         $ultimas = array_reverse($ultimas);
 
         $contador = 0;
+
         foreach ($ultimas as $cotacao) {
             $ultimo_valor = (int) ($contador == 0) ? $ultimas[0]->high24hr : $ultimas[$contador-1]->last;
             if ($cotacao->last > $ultimo_valor) {
-                $subindo++;
-                echo '<span style="color:green">' . $cotacao->last . ' > ' . $ultimo_valor . ' -- subindo: ' . $subindo . '</span><br>';
+                $pontuacao = $this->pontuacao($ultimo_valor, $cotacao->last);
+                $subindo = $subindo + $pontuacao;
+                echo $cotacao->last . ' > ' . $ultimo_valor;
+                echo ' | Subindo: &nbsp;&nbsp;&nbsp;' . $subindo;
+                echo ' | Pontos: ' . $pontuacao . '<br>';
             } else if ($cotacao->last < $ultimo_valor) {
-                $descendo++;
-                echo '<span style="color:red; font-weight:bold;">' . $cotacao->last . ' < ' . $ultimo_valor . ' -- descendo: ' . $descendo . '</span><br>';
+                $pontuacao = $this->pontuacao($cotacao->last, $ultimo_valor);
+                $descendo = $descendo + $pontuacao;
+                echo $cotacao->last . ' < ' . $ultimo_valor;
+                echo ' | Descendo: ' . $descendo;
+                echo ' | Pontos: ' . $pontuacao . '<br>';
             }
             $valor_final = $cotacao->last;
             $contador++;
@@ -59,35 +69,35 @@ class Coin extends CI_Controller {
         echo '----------------------------------------------------<br>';
         $amount_wallet = $this->walletM->get_by_id(1);
         if ($subindo > $descendo && $ultimas[0]->last <= $valor_final) {
-            $percent = $valor_final * 0.02;
-            if (($valor_final - $percent) > $ultimas[0]->last) {
+            $percent_value = $valor_final * $percent;
+            if (($valor_final - $percent_value) > $ultimas[0]->last) {
                 echo 'Hora de vender...'  . '<br>';
-                // Se tiver mais de 2 moedas na carteira
-                if ($amount_wallet->amount_coin >= 2) {
+                // Se tiver moedas na carteira
+                if ($amount_wallet->amount_coin >= $qtd) {
                     $insert = $this->tradeM->insert(
                         [
                             'value'  => $valor_final,
-                            'amount' => 2,
+                            'amount' => $qtd,
                             'type'   => 'Vender'
                         ]
                     );
-                    $update_wallet = $this->walletM->update(1, ['amount_coin' => ($amount_wallet->amount_coin - 2), 'amount_usd' => ($amount_wallet->amount_usd + ($valor_final * 2))]);
+                    $update_wallet = $this->walletM->update(1, ['amount_coin' => ($amount_wallet->amount_coin - $qtd), 'amount_usd' => ($amount_wallet->amount_usd + ($valor_final * $qtd))]);
                 }
             }
         } else if ($descendo > $subindo && $ultimas[0]->last >= $valor_final) {
-            $percent = $valor_final * 0.02;
-            if (($valor_final + $percent) < $ultimas[0]->last) {
+            $percent_value = $valor_final * $percent;
+            if (($valor_final + $percent_value) < $ultimas[0]->last) {
                 echo 'Hora de comprar...' . '<br>';
                 // Se tiver dinheiro suficiente na carteira
-                if ($amount_wallet->amount_usd >= ($valor_final * 2)) {
+                if ($amount_wallet->amount_usd >= ($valor_final * $qtd)) {
                     $insert = $this->tradeM->insert(
                         [
                             'value'  => $valor_final,
-                            'amount' => 2,
+                            'amount' => $qtd,
                             'type'   => 'Comprar'
                         ]
                     );
-                   $update_wallet = $this->walletM->update(1, ['amount_coin' => ($amount_wallet->amount_coin + 2), 'amount_usd' => ($amount_wallet->amount_usd - ($valor_final * 2))]);
+                   $update_wallet = $this->walletM->update(1, ['amount_coin' => ($amount_wallet->amount_coin + $qtd), 'amount_usd' => ($amount_wallet->amount_usd - ($valor_final * $qtd))]);
                 }
             }
         } else {
@@ -95,8 +105,36 @@ class Coin extends CI_Controller {
         }
         // echo 'Maior valor de 24 horas: ' . $ultimas[0]->high24hr . '<br>';
         echo 'Totais: Descendo: ' . $descendo . ' - Subindo: ' . $subindo . '<br>';
-        echo 'Primeiro valor: ' . $ultimas[0]->last . '<br>';
-        echo 'Último valor: ' . $valor_final . '<br>';
-        echo 'Diferença de %' . $percent;
+        echo 'Primeiro valor: US$' . $ultimas[0]->last . '<br>';
+        echo 'Último valor: US$' . $valor_final . '<br>';
+        if (isset($percent_value)) {
+            echo 'Diferença de US$' . $percent_value;
+        }
+    }
+
+    private function pontuacao($menor, $maior)
+    {
+        $porcentagem = 100 - (($menor / $maior) * 100);
+        switch ($porcentagem) {
+            case ($porcentagem > 0 && $porcentagem <= 1):
+                return 1;
+                break;
+            case ($porcentagem > 1 && $porcentagem <= 3):
+                return 3;
+                break;
+            case ($porcentagem > 3 && $porcentagem <= 7):
+                return 6;
+                break;
+            case ($porcentagem > 7 && $porcentagem <= 10):
+                return 10;
+                break;
+            case ($porcentagem > 10):
+                return 50;
+                break;
+            default:
+                return 1;
+                break;
+        }
+
     }
 }
